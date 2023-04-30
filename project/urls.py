e15906a9f3012a00c3111ace8d602a55c791d1b5
Abdullah-hanini <98618536+Abdullah-hanini@users.noreply.github.com>
2023-04-30 22:17:53 +0300

    made

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls' , namespace='accounts')),
     path('admin/', admin.site.urls),
+    path('product/', include('products.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
