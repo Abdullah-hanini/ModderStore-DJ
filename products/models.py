a2a89d43383e8bd7e5df6fd95d13364657ceb83c
Abdullah-hanini <98618536+Abdullah-hanini@users.noreply.github.com>
2023-05-07 16:54:51 +0000

    tt

--- products/models.py
+++ products/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.utils.text import slugify
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+
+
 '''
 Editions = (
     ('Standard Edtion','Standard Edtion'),
@@ -38,3 +42,5 @@ class Category (models.Model):
     def __str__(self):
         return self.name   
 
+class Orders(models.Model) :
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
